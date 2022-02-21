cc994238f795c4363109ca2cd4fb56047c3d7c40
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2022-02-20 20:33:35 -0700

    Added navigation dots and social media icons to About section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper';
 
 import { images } from '../../constants';
 import './About.scss';
@@ -54,4 +55,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default AppWrap(About, 'about');
