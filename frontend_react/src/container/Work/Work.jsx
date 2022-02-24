529a09157e4fb4e750c545594aa13ad6aaddba4a
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2022-02-23 17:01:49 -0700

    Added templates for Portfolio projects section in work.jsx

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion} from 'framer-motion';
+import { AppWrap } from '../../wrapper';
+import { urlFor, client} from '../../client';
+import './Work.scss';
+
+
 
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setactiveFilter] = useState('All');
+
+  const handleWorkFilter = (item) => {
+
+  }
   return (
-    <div>Work</div>
-  )
+      <>
+      <h2 className='head-text'> My Creative<span> Portfolio </span>Projects</h2>
+
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web Apps', 'Mobile Apps', 'React JS', 'All'].map((item, index) => 
+        (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+
+      </div>
+      </>
+    )
 }
 
 export default Work
