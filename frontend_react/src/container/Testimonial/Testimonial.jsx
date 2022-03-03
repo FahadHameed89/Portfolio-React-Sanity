ea7fbcaf95d8eef32e5b5ca32a1c9e125a74509b
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2022-03-02 21:22:45 -0700

    More Portfolio notes

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query).then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
+const currentTestimonialIndex = testimonials[currentIndex];
+
   return (
-    <div>Testimonial</div>
+    <>
+        {testimonials.length && (
+          <>
+          <div className='app__testimonial-item app__flex'>
+            <img src={urlFor(currentTestimonialIndex.imgurl)} alt="testimonial" />
+            <div className='app__testimonial-content'>
+                <p className='p-text'>{currentTestimonialIndex.feedback}</p>
+                <div>
+                  <h4 className='bold-text'>{currentTestimonialIndex.name}</h4>
+                  <h5 className='p-text'>{currentTestimonialIndex.company}</h5>
+                </div>
+            </div>
+          </div>
+          </>
+        )}
+        
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  "app__primarybg"
+  );
